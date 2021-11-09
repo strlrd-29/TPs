6212220180ee189bd811200ea2c6c4ae5dfff5f3
strlrd-29 <ouassimghribi@gmail.com>
2021-11-09 19:22:41 +0100

    setup

---
+++ functions.h
@@ -0,0 +1,18 @@
+#include <cstdlib>
+#include <stdlib.h>
+typedef  struct Element Element;
+struct Element {
+	int nombre;
+	Element *suivant;
+};
+
+typedef struct Liste Liste;
+struct Liste{
+	Element *premier;
+	Element *dernier;
+};
+
+
+Liste *initialisationListe(int val);
+void ajout_deb(Liste *l, int val);
+void supp_deb(Liste *l);
