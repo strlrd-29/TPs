16a200b68c9240cae0c8c6dce0bd155b5e644294
strlrd-29 <ouassimghribi@gmail.com>
2021-11-26 22:51:49 +0100

    completed all given functions

--- main.cpp
+++ main.cpp
@@ -1,76 +1,117 @@
 #include <iostream>
-#include <string>
 #include <vector>
 
 using namespace std;
 
 struct note {
-  int maximum_grad = 20;
+  int MAXIMUM_GRADE = 20;
   double value;
 };
 
 typedef note Note;
 
+double getValue(Note note) { return note.value; }
+
+Note note_moyeene(vector<Note> notes) {
+  Note noteMoyenne;
+  double somme;
+  for (int i = 0; i < notes.size(); i++) {
+    somme += notes[i].value;
+  }
+  noteMoyenne.value = somme / notes.size();
+  return noteMoyenne;
+}
+
 struct result {
-  string nom_module;
-  Note note;
+  string module;
+  Note lanote;
 };
 
 typedef result Result;
 
+Note getNote(Result res) { return res.lanote; }
+
 struct student {
-  string first_name;
-  string last_name;
+  string firstName;
+  string lastName;
   vector<Result> results;
 };
 
 typedef student Student;
 
-// avoir la valeur de la note
-double getValue(Note note) { return note.value; }
-
-// avoir la note moyenne
-note noteMoyenne(vector<Note> notes) {
-  note note_moy;
-  double value_moy;
-  for (int i = 0; i < notes.size(); i++) {
-    value_moy += notes[i].value;
-  }
-  note_moy.value = value_moy / notes.size();
-  return note_moy;
-}
-
-// avoir la note
-Note getNote(Result res) { return res.note; }
-
-// ajouter un resultat
 void addResult(string module, Note note, vector<Result> &results) {
-  Result current;
-  current.nom_module = module;
-  current.note.value = note.value;
-  results.push_back(current);
+  Result newRes;
+  newRes.module = module;
+  newRes.lanote = note;
+  results.push_back(newRes);
 }
 
-// avoir un vecteur des notes
 vector<Note> getNotes(Student student) {
   vector<Note> notes;
   for (int i = 0; i < student.results.size(); i++) {
-    notes.push_back(student.results[i].note);
+    notes.push_back(student.results[i].lanote);
   }
   return notes;
 }
 
-// avoir la note moyenne
 double averageNote(Student student) {
-  Note note_moy = noteMoyenne(getNotes(student));
-  return note_moy.value;
+  return note_moyeene(getNotes(student)).value;
+}
+
+void printResult(Student student) {
+  cout << student.lastName + " " + student.firstName << endl;
+  for (int i = 0; i < student.results.size(); i++) {
+    cout << student.results[i].module << " : "
+         << student.results[i].lanote.value << endl;
+  }
+  cout << "La note moyenne est : " << averageNote(student) << endl;
+}
+
+struct promotion {
+  string name;
+  vector<Student> students;
+};
+
+typedef promotion Promotion;
+
+void addStudent(promotion &promo, Student student) {
+  promo.students.push_back(student);
+}
+
+vector<Student> getStudents(Promotion promo) { return promo.students; }
+
+void printStudentResults(Promotion promo) {
+  cout << promo.name << endl;
+  for (int i = 0; i < promo.students.size(); i++) {
+    printResult(promo.students[i]);
+    cout << endl;
+  }
 }
 
 int main() {
-  vector<Note> notes(3);
-  notes[0].value = 15;
-  notes[1].value = 9;
-  notes[2].value = 5;
-  cout << "La note moyenne est egale a " << noteMoyenne(notes).value << endl;
+
+  Promotion promo;
+  vector<Student> students;
+  Student student1;
+  student1.firstName = "Ouassim Abdelmalek";
+  student1.lastName = "Ghribi";
+  Note note11;
+  note11.value = 16;
+  Note note12;
+  note12.value = 13;
+  addResult("math", note11, student1.results);
+  addResult("info", note12, student1.results);
+  Student student2;
+  student2.firstName = "Paul";
+  student2.lastName = "Calcul";
+  Note note21;
+  note21.value = 12;
+  Note note22;
+  note22.value = 17;
+  addResult("Programmation 2", note21, student2.results);
+  addResult("TNS", note22, student2.results);
+  addStudent(promo, student1);
+  addStudent(promo, student2);
+  printStudentResults(promo);
   return 0;
 }
